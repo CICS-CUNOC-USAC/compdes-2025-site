34f888d5449c8a0e60f0061d0aed28d696d744d5
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-05-26 18:01:05 -0600

    feat: Add missing general info, add new icons and custom components for General Info Summaries, start nav menu refactor

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -61,7 +61,7 @@
   let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 200) {
         selectHeader.classList.add('header-scrolled')
       } else {
         selectHeader.classList.remove('header-scrolled')
